b1903296ca4ef55347a7c7c90fd5c25c88ff02f5
Mehedi Khan Rakib <mkrakib007@gmail.com>
2021-02-07 20:22:52 +0600

    problem-solving

--- assignment.js
+++ assignment.js
@@ -1,34 +1,46 @@
+// https://github.com/rakib8799/problem_solving_assignment
+
+// Problem-1
 const kilometerToMeter = function (kilometer) {
     if (kilometer >= 0) return kilometer * 1000;
-    else return `Negative number`;
+    else return `Distance can't be negative`;
 };
-const inputKilometer = kilometerToMeter(3);
+const inputKilometer = kilometerToMeter(5);
 console.log(inputKilometer);
 
-let sum = 0;
+// Problem-2
 const budgetCalculator = function (watch, phone, laptop) {
-    sum += 50 * watch + 100 * phone + 500 * laptop;
-    return sum;
+    let sumOfItems = 0;
+    sumOfItems += 50 * watch + 100 * phone + 500 * laptop;
+    return sumOfItems;
 };
-const inputValues = budgetCalculator(5, 3, 2);
-console.log(inputValues);
+const inputItems = budgetCalculator(5, 3, 2);
+console.log(inputItems);
 
+// Problem-3
 const hotelCost = function (numberOfDays) {
-    if (numberOfDays >= 1 && numberOfDays <= 10) return numberOfDays * 100;
-    else if (numberOfDays >= 11 && numberOfDays <= 20)
-        return numberOfDays * (100 - 20);
-    else return numberOfDays * (100 - 50);
-};
-const inputHotel = hotelCost(19);
-console.log(inputHotel);
-
-const megaFriend = function (friend) {
-    let firstValue = friend[0];
-    for (let i = 0; i < friend.length; i++) {
-        let element = friend[i];
-        if (element.length > firstValue.length) firstValue = element;
+    let costOfLiving = 0;
+    for (let i = 1; i <= numberOfDays; i++) {
+        if (i <= 10) costOfLiving += 100;
+        else if (i <= 20) costOfLiving += 100 - 20;
+        else costOfLiving += 100 - 50;
     }
-    return firstValue;
+    return costOfLiving;
 };
-const inputFriend = megaFriend(['Rakib', 'Mehedi', 'Rabbi', 'Raihan', 'Sufi']);
-console.log(inputFriend);
+const inputDays = hotelCost(14);
+console.log(inputDays);
+
+// Problem-4
+const megaFriend = function (friends) {
+    let firstElement = friends[0];
+    for (let i = 0; i < friends.length; i++) {
+        let restOfTheElements = friends[i];
+        if (restOfTheElements.length > firstElement.length)
+            firstElement = restOfTheElements;
+        else if (restOfTheElements.length == 0)
+            return `Name field can't be empty`;
+    }
+    return firstElement;
+};
+const inputFriends = megaFriend(['Rakib', 'Mehedi', 'Rabbi', 'Raihan', 'Sufy']);
+console.log(inputFriends);
